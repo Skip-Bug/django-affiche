26e2417a66be782e466b23855709918f0a26c335
Leonid Stoyko <gribojor@gmail.com>
2026-07-01 13:34:10 +0300

    add places model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
 # Create your models here.
-print("hello!")
+class Place(models.Model):
+    title = models.CharField("Название", max_length=200)
+    description_short = models.TextField(
+        "Краткое описание",
+        blank=True,
+        null=True
+        )
+    description_long = models.TextField(
+        "Полное описание",
+        blank=True,
+        null=True
+        )
+    
+    lng = models.FloatField('Долгота', null=True, blank=True)
+    lat = models.FloatField('Широта', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
